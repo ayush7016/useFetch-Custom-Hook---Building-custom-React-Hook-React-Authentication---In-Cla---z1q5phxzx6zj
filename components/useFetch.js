0632aff366bd7ac51f0ad9143086cee9ed022071
ayush7016 <79650268+ayush7016@users.noreply.github.com>
2023-10-15 10:33:45 +0530

    Update useFetch.js

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 //write your code here
 
+ async function callApiHere(setLoading,setData,setError,url){
+    try{
+        const response=await axios.get(url);
+        setData(response.data);
+    }
+    catch(errorMessage){
+        setError(errorMessage)
+    }
+    setLoading(false);
+}
+
+function useFetch(url){
+ const [data,setData]=useState(null);
+ const [loading,setLoading]=useState(true);
+ const [error,setError]=useState(null);
+
+ useEffect(()=>{
+    callApiHere(setLoading,setData,setError,url);
+ },[url])
+ return {data,loading,error}
+}
 export default useFetch
